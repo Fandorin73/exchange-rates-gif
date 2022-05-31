e6b461200d86e052137f90272aeb45b785c193fd
Fandorin73 <monten49@gmail.com>
2022-05-31 17:00:17 +0300

    Сreating an application framework

---
+++ src/main/java/com/alfabank/exchangeratesgif/ExchangeRatesGifApplication.java
@@ -0,0 +1,13 @@
+package com.alfabank.exchangeratesgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesGifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alfabank/exchangeratesgif/ExchangeRatesGifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfabank.exchangeratesgif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesGifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
